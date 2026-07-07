c1f7b89161b19c2af7e902f717757b69c18e7484
yeshie schischa <schischayeshie@gmail.com>
2026-07-07 14:47:35 +0200

    need to make a double click to delete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const saveBtn = document.getElementById("save")
 const allNotes = [];
 const succesMsg = document.getElementById("succesMsg");
 const noteContanier = document.getElementById("note-contanier")
+let notes;
 add.addEventListener("click", () => {
   form.style.display = "flex"
 })
@@ -11,17 +12,29 @@ saveBtn.addEventListener("click", () => {
     allNotes.push({
         title: form.querySelector("input").value,
         body:  form.querySelector("textarea").value,
+        checked: false,
+        time: new Date().getDate() + "/" + (new Date().getMonth() + 1) + "/" + new Date().getFullYear()
     });
      form.style.display = "none"
      succesMsg.style.display = "block"
      noteContanier.innerHTML = viewNotes(allNotes)
      form.querySelector("input").value = "";
      form.querySelector("textarea").value = "";
-})
+     setTimeout(() => {
+        succesMsg.style.display = "none"
+     }, 2000)
+     
+      notes = document.querySelectorAll(".note");
+      console.log(notes)
+      notes.forEach((each, index) => {
+        each.addEventListener("dblclick", () => {
+          allNotes.splice(index, 1);
+          noteContanier.innerHTML = viewNotes(allNotes)
+          console.log(each, index)
+        })
+      })
+    })
 const viewNotes = (arr) => {
-  return arr.map(each => `<div class="note"><h2>${each.title}</h2><p>${each.body}</p></div>`).join("")
+  return arr.map(each => `<div class="note"><h2>${each.title}</h2><p>${each.body}</p><input type="checkbox" ${each.checked ? "checked" : ""}><span>${each.time}</span></div>`).join("")
 }
-const view = document.getElementById("view");
-view.addEventListener("click", () => {
-    console.log(allNotes)
-})
+console.log(allNotes);
