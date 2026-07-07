acca99ac7247a3b81384b65d23f736b5186dc069
Yeshie-Schischa <schischayeshie@gmail.com>
2026-07-07 09:53:44 +0000

    /,,

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const add = document.getElementById("add");
 const form = document.querySelector("form");
 const saveBtn = document.getElementById("save")
 const allNotes = [];
+const succesMsg = document.getElementById("succesMsg");
+const noteContanier = document.getElementById("note-contanier")
 add.addEventListener("click", () => {
   form.style.display = "flex"
 })
-saveBtn.addEventListener("click", () => {
-    
- 
-    console.log(noteObj)
+saveBtn.addEventListener("click", () => { 
     allNotes.push({
         title: form.querySelector("input").value,
         body:  form.querySelector("textarea").value,
     });
+     form.style.display = "none"
+     succesMsg.style.display = "block"
+     noteContanier.innerHTML = viewNotes(allNotes)
+     form.querySelector("input").value = "";
+     form.querySelector("textarea").value = "";
 })
+const viewNotes = (arr) => {
+  return arr.map(each => `<div class="note"><h2>${each.title}</h2><p>${each.body}</p></div>`).join("")
+}
 const view = document.getElementById("view");
 view.addEventListener("click", () => {
     console.log(allNotes)
