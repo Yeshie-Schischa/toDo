f44995dd82e91dad00e0e4d66a3791bcfa1a16a2
Yeshie-Schischa <schischayeshie@gmail.com>
2026-07-14 17:16:05 +0000

    save

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const allNotes = [];
 const succesMsg = document.getElementById("succesMsg");
 const noteContanier = document.getElementById("note-contanier")
 let notes;
-const createOptions = document.createElement("ul");
-createOptions.appendChild(document.createElement("li")).innerHTML = "Edit";
-createOptions.appendChild(document.createElement("li")).innerHTML = "Delete";
-createOptions.classList.add("note-menu")
+
 
 add.addEventListener("click", () => {
   form.style.display = "flex"
@@ -30,17 +27,39 @@ saveBtn.addEventListener("click", () => {
      }, 2000)
     })
 
-noteContanier.addEventListener("adsf", (e) =>{
+/*noteContanier.addEventListener("adsf", (e) =>{
   console.log(e.target.closest(".note"))
   allNotes.splice(e.target.closest(".note").id.slice(1), 1)
   noteContanier.innerHTML = viewNotes(allNotes)
-})
+})*/
 const viewNotes = (arr) => {
- return  arr.map((each, index, array) => `<div id="a${index}" class="note"><h2>${each.title}</h2><p>${each.body}</p><input type="checkbox" ${each.checked ? "checked" : ""}><div class="menu"><span>☰</span><div class="positioned"></div></div><span class="time">${each.time}</span></div>`).join("")
+ return  arr.map((each, index, array) => `<div id="a${index}" class="note">
+            <h2>${each.title}</h2>
+            <p>${each.body}</p>
+            <input type="checkbox" ${each.checked ? "checked" : ""}>
+            <div class="menu">
+              <span>˅</span>
+               <ul class="menu-list">
+                <li class="edit">Edit</li>
+                <li class="delete">Delete</li>
+                <li class="cancel">Cancel</li>
+               </ul>
+            </div>
+           <span class="time">${each.time}</span>
+          </div>`).join("")
 }
-noteContanier.addEventListener("click", (e) => {
+/*noteContanier.addEventListener("click", (e) => {
 if(e.target.closest(".menu")) {
   console.log(e.target.lastChild)
   e.target.lastChild.appendChild(createOptions)
 }
+})*/
+noteContanier.addEventListener("click", (e) => {
+  if(e.target.closest(".menu")) {
+    e.target.closest(".menu").parentElement.classList.add("clicked")
+    e.target.closest(".menu").querySelector("span").classList.add("viewit")
+    console.log(e.target.closest(".menu").querySelector("span").style.display)
+    e.target.closest(".menu").querySelector(".menu-list").style.display = "block"
+    
+  }
 })
